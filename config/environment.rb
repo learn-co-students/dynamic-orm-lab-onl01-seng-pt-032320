b3fd6793b691cb489c70f4d6dec00f535af79940
Curtis <curtisdevloper@gmail.com>
2020-05-20 01:01:54 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,11 @@ DB[:conn].execute("DROP TABLE IF EXISTS students")
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS students (
-  id INTEGER PRIMARY KEY, 
-  name TEXT, 
-  grade INTEGER
+    
+    id INTEGER PRIMARY KEY, 
+    name TEXT,
+    grade INTEGER
+  
   )
 SQL
 
