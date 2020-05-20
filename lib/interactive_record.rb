b64eda368f7c77d8d6d53aaa37c9ec7977cb7c44
Curtis <curtisdevloper@gmail.com>
2020-05-20 09:20:23 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,11 +51,12 @@ class InteractiveRecord
         sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
         DB[:conn].execute(sql)
       end
-    
-      def self.find_by(attribute_hash)
-        value = attribute_hash.values.first
-        formatted_value = value.class == Fixnum ? value : "'#{value}'"
-        sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
-        DB[:conn].execute(sql)
+     
+      def self.find_by(attribute)
+        column_name = attribute.keys[0].to_s
+        value_name = attribute.values[0]
+        sql = "SELECT * FROM #{table_name} WHERE #{column_name} = ?"
+        DB[:conn].execute(sql,value_name)
       end
+      
 end
